bca0bce7ed25c14d35f8f4faefa1f6815acb1689
Vitor Dorea <vitordanniel@hotmail.com>
2026-02-17 22:13:39 -0300

    criacao da pasta python poo, ex000 e ex001 ex002

---
+++ exercicios/ex002/ex002.py
@@ -0,0 +1,31 @@
+# Declaração de Classe
+class Gafanhoto:
+    """
+Essa classe cria um Gafanhoto, que é uma pessoa que tem nome e idade.
+Para criar uma nova pessoa, use
+variavel = Gafanhoto(nome, idade)
+    """
+    def __init__(self, nome = "vazio", idade = 0):  # Métdo Construtor
+        # Atributos de Instância
+        self.nome = nome
+        self.idade = idade
+
+    # Métodos de Instância
+    def aniversario(self):
+        self.idade += 1  # ou seja self.idade = self.idade + 1
+
+    def __str__(self): # dunder method para exibição personalizada
+        return f"{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade."
+
+    def __getstate__(self): # dunder method
+        return f"Estado: nome = {self.nome} ; idade = {self.idade}"
+
+# Declaração de Objetos
+g1 = Gafanhoto("Maria",17)
+g1.aniversario()
+print(g1)
+print(g1.__doc__) # Dunder Attribute
+print(g1.__dict__) # Attribute
+print(g1.__getstate__()) # Method
+print(g1.__class__) # Attribute
+
